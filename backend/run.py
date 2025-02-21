d5bf5149365bfcc7913054916df44bb40e9fc814
RocksPers4 <amiruriel@hotmail.com>
2025-02-21 12:22:33 -0500

    Proyecto_Pochibot_V4

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,5 @@
 import os
-from backend.app import app
+from backend import app
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 4000))  # Usa 4000 si no hay otra variable de entorno

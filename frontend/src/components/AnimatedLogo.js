b99945855f53780364be6e22a11a2f1cbf749196
RocksPers4 <amiruriel@hotmail.com>
2025-06-09 12:00:51 -0500

    Configuración principal

--- frontend/src/components/AnimatedLogo.js
+++ frontend/src/components/AnimatedLogo.js
@@ -1,11 +1,9 @@
-import React from 'react';
-
 const AnimatedLogo = () => {
   return (
     <div className="flex items-center">
       {/* Reemplaza 'ruta_a_tu_logo_animado' con la ruta real a tu archivo */}
       <img 
-        src="./frontend/src/Logo_chat.gif" 
+        src="./frontend/src/pochi.png" 
         alt="Logo animado" 
         className="w-10 h-10 mr-2"
       />
